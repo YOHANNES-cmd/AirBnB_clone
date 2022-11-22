1a62a2ec79f59bb99db1d3da3cdc36583f207452
Okinbird <okinyinka@yahoo.co.uk>
2022-11-22 14:30:12 -0800

    storage mode

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
     and methods for other classes
 """
 import models
+import json
 import uuid
 from uuid import uuid4
 from datetime import datetime
@@ -24,6 +25,14 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, dform)
+                else:
+                    self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """str() representation of the BaseModel instance."""
@@ -33,6 +42,8 @@ class BaseModel:
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.today()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
