b2a9d9e714abf2ab66b2dcba5466373e9c22e2cc
Okinbird <okinyinka@yahoo.co.uk>
2022-11-22 11:24:40 -0800

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -24,14 +24,6 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, dform)
-                else:
-                    self.__dict__[key] = value
-        else:
-            models.storage.new(self)
 
     def __str__(self):
         """str() representation of the BaseModel instance."""
@@ -41,7 +33,6 @@ class BaseModel:
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.today()
-        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
