e3ef94b5bce46ebd5ab291fdd96862a742bc500b
Okinbird <okinyinka@yahoo.co.uk>
2022-11-26 10:59:00 -0800

    update files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,8 +4,6 @@
     and deserializes JSON file to instances
 """
 import json
-import os.path
-from os.path import exists
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -27,24 +25,24 @@ class FileStorage:
 
     def all(self):
         """Return the dictionary __objects."""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id"""
         ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        self.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
-        odict = FileStorage.__objects
+        odict = self.__objects
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w", encoding="UTF-8") as file:
+        with open(self.__file_path, "w", encoding="UTF-8") as file:
             json.dump(objdict, file)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path, "r", encoding="UTF-8") as file:
+            with open(self.__file_path, "r", encoding="UTF-8") as file:
                 objdict = json.load(file)
                 for o in objdict.values():
                     cls_name = o["__class__"]
