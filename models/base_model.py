1aeece3eb8eae50d0944bea6391f3e31719e468e
Okinbird <okinyinka@yahoo.co.uk>
2022-11-26 07:38:42 -0800

    file update

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k == "created_at" or key == "updated_at":
+                if k == "created_at" or k == "updated_at":
                     self.__dict__[k] = datetime.strptime(v, dform)
                 else:
                     self.__dict__[k] = v
