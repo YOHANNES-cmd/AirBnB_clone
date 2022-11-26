c24ee9a44f09da5d877858a945e9db05d3a199b4
Okinbird <okinyinka@yahoo.co.uk>
2022-11-26 07:43:57 -0800

    file update

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 
 storage = FileStorage()
