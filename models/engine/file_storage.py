16aa9fb942e63f738ae37c8d38da6dc1b5338050
Okinbird <okinyinka@yahoo.co.uk>
2022-11-26 08:05:10 -0800

    file update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,14 +36,14 @@ class FileStorage:
         """Serialize __objects to the JSON file __file_path."""
         odict = FileStorage.__objects
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w", encoding="UTF-8") as file:
-            json.dump(objdict, file)
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path, "r", encoding="UTF-8") as file:
-                objdict = json.load(file)
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
                 for o in objdict.values():
                     cls_name = o["__class__"]
                     del o["__class__"]
